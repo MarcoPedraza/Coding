08c06101c8b0ac7d7dfd0acb7bc1022709015fe4
MarcoPedraza <marcopedraza@MacBook-Pro-de-Marco.local>
2024-02-21 23:49:22 +0100

    Modificado el archivo python de el curso de Dalto

--- PY/curso_dalto/datos-compuestos.py
+++ PY/curso_dalto/datos-compuestos.py
@@ -1,11 +1,27 @@
+#Creando una lista se pueden modificar
 lista = ["Lucas Dalto","Soy Dalto",True,1.85]
+
+#Creando una tupla no se puede modificar
 tupla = ("Lucas Dalto","Soy Dalto",True,1.85)
 
-#La tupla no se puede modificar mientras que la lista si 
 
 lista[3] = "Maquinola"
 
 print(lista)
 print(lista[0])
 
-print
+#Creando un conjunto (set) ; se pueden intercambiar
+    # No puedo sacar valores individualmente y no se pueden repetir valores
+conjunto = {"Lucas Dalto","Soy Dalto",True,1.85}
+
+# Para ver cuales son los elementos del set tenemos que recorrerlo con un bucle
+
+diccionario = {
+    "Nombre" : "Dalto",
+    "Canal" : "Soy Dalto",
+    "esta emocionado" : True,
+    "dato duplicado" : "Soy Dalto"
+}
+
+print(diccionario["Canal"])
+
